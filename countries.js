dbfbd0de2aa6606f241a54bf4c5fe36a685fde7d
kovacs1balazs <88842494+kovacs1balazs@users.noreply.github.com>
2021-10-20 18:14:54 +0200

    countries added

---
+++ countries.js
@@ -0,0 +1,9 @@
+function loadCountry() {
+    fetch("https://restcountries.com/v3.1/name/peru").then(r => r.json()).then(showCountry);
+}
+function showCountry(country){
+    console.log(country);
+    const name = country[0].name.official;
+    document.getElementById("name").textContent = name;
+}
+window.addEventListener("load", loadCountry);
